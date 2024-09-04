6e0c852465bf0df3cb399de0c993df6f1d1dc3e7
Siam Bhuiyan <bhuiyansiam077@gmail.com>
2024-09-04 14:30:56 +0600

    feat: add custom user model

--- real_state/settings/development.py
+++ real_state/settings/development.py
@@ -2,11 +2,11 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': env("POSTGRES_ENGINE"),
-        'NAME': env('POSTGRES_NAME'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('PG_HOST'),
-        'PORT': env('PG_PORT'),
+        'ENGINE': env('POSTGRES_ENGINE'),  # Matches .env variable
+        'NAME': env('POSTGRES_DB'),        # Matches .env variable
+        'USER': env('POSTGRES_USER'),      # Matches .env variable
+        'PASSWORD': env('POSTGRES_PASSWORD'),  # Matches .env variable
+        'HOST': env('PG_HOST'),            # Matches .env variable
+        'PORT': env('PG_PORT'),            # Matches .env variable
     }
 }
